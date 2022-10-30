f984db7d8284700e1deadda5ef5d53dfff2f6f6c
devWeibel <weibel.jason@gmail.com>
2022-10-30 17:22:10 -0400

    finished new user registration post

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
 // Login a User from Landing page (default '/'). Render the Account page.
 router.get('/', (req, res) => {
@@ -11,7 +12,50 @@ router.post('/', (req, res, next) => {
 
 // Register a new User from the Landing page. Render the Account page.
 router.post('/', (req, res) => {
-    res.render('account')
+    const {username, email, password, password2} = req.body;
+    let error = [];
+    
+    // make sure all field have a value
+    if(!username || !email || !password || !password2) {
+        error.push({ msg: "Please complete all fields." })
+    };
+
+    // check if passwords match.
+    if(password !== password2) {
+        error.push({ msg: "Passwords do not match." })
+    };
+
+    // check password length.
+    if(password.length < 8) {
+        error.push({ msg: "Password must be at least 8 characters long." })
+    };
+
+    // display an error message if there is one.
+    if(error.length > 0) {
+        res.render('landing', {
+            error: error,
+            username: username,
+            email: email,
+            password: password,
+            password2: password2
+        })
+    } else {
+        // check if user with that email already exists.
+        User.findOne({ email : email }).exec((err, user) => {
+            console.log(user);
+            if(user) {
+                error.push({ msg: "A user with that email already exists." })
+            // pass.    
+            } else {
+                const newUser = new User({
+                    username : username, 
+                    email : email,
+                    password : password
+                });
+            };
+        });
+    };
+    res.render('account');
 });
 
 // Logout a User from the Account Page (button). Render the Landing page.
